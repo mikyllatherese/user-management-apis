d0166a503dd23bdc4d77f53027b6077d002e85c4
Cheeeswezzz <itguyxyz@gmail.com>
2025-03-28 02:16:49 +0800

    resolved

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,8 @@
-//List all users (GET request)
-router.get("/", async (req, res) => {
-  console.log(" Fetching all users...");
-  const userRepository = AppDataSource.getRepository(User);
+import * as express from "express";
+import { AppDataSource } from "../config/database";
+import { User } from "../entities/User";
 
-  try {
-    const users = await userRepository.find();
-    console.log(" Users found:", users);
-    res.status(200).json(users);
-  } catch (error) {
-    console.error(" Error retrieving users:", error);
-    res.status(500).json({ error: "Error retrieving users" });
-  }
-});
+const router = express.Router();
+
+
+export default router;
