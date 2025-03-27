bad3d3ae38c93f998bab5b93b1aa48ee949b6440
unknown <YY310@ccs.local>
2025-03-27 02:32:09 -0700

    resolved

---
+++ src/entities/User.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name!: string;
+
+    @Column({ unique: true })
+    username!: string;
+
+    @Column({ unique: true })
+    email!: string;
+
+    @Column()
+    password!: string;
+}
